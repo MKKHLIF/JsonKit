3371aaa7683443920b71ee992c007342564dcb02
Mayssam-Khlif <khlif.mk@proton.me>
2024-12-03 22:36:39 +0100

    initial commit

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/mk/Desktop/JsonKit
+# Build directory: C:/Users/mk/Desktop/JsonKit/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("examples")
+subdirs("tests")
+subdirs("external/unity")
---
+++ include/value.h
@@ -0,0 +1,8 @@
+//
+// Created by mk on 03/12/2024.
+//
+
+#ifndef VALUE_H
+#define VALUE_H
+
+#endif //VALUE_H
